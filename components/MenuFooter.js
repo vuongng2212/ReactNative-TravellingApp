c9e6f77533702efdd449978002e3dd9f760c7127
VuongNguyen <kudoyaung@gmail.com>
2024-11-20 21:50:01 +0700

    [UPDATE] alignItem for Icon in MenuFooter

--- components/MenuFooter.js
+++ components/MenuFooter.js
@@ -4,35 +4,50 @@ import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 export default function MenuFooter({ navigation }) {
   return (
     <View style={styles.footer}>
-      <TouchableOpacity onPress={() => navigation.navigate("SearchScreen")}>
+      <TouchableOpacity
+        style={styles.footerNavItem}
+        onPress={() => navigation.navigate("SearchScreen")}
+      >
         <Image
           style={styles.menuImg2}
           source={require("../assets/SearchIcon.png")}
         />
         <Text style={styles.footerText}>Search</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("FavoriteScreen")}>
+      <TouchableOpacity
+        style={styles.footerNavItem}
+        onPress={() => navigation.navigate("FavoriteScreen")}
+      >
         <Image
           style={styles.menuImg2}
           source={require("../assets/heart.png")}
         />
         <Text style={styles.footerText}>Favorite</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("BookingScreen")}>
+      <TouchableOpacity
+        style={styles.footerNavItem}
+        onPress={() => navigation.navigate("BookingScreen")}
+      >
         <Image
           style={styles.menuImg2}
           source={require("../assets/booking.png")}
         />
         <Text style={styles.footerText}>Booking</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("InboxScreen")}>
+      <TouchableOpacity
+        style={styles.footerNavItem}
+        onPress={() => navigation.navigate("InboxScreen")}
+      >
         <Image
           style={styles.menuImg2}
           source={require("../assets/message.png")}
         />
         <Text style={styles.footerText}>Inbox</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("ProfileScreen")}>
+      <TouchableOpacity
+        style={styles.footerNavItem}
+        onPress={() => navigation.navigate("ProfileScreen")}
+      >
         <Image
           style={styles.menuImg2}
           source={require("../assets/profile.jpg")}
@@ -62,5 +77,8 @@ const styles = StyleSheet.create({
     height: 20,
     margin: 3,
     justifyContent: "center",
+  },
+  footerNavItem: {
+    alignItems: "center",
   },
 });
